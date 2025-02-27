3f5052a79f900619fb7f7c2576eea6c004327ae7
Tec <Tec@SKAB3-PC1>
2025-02-27 12:37:03 +0100

    Add project files.

---
+++ publisherproject/Controllers/AuthorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace publisherproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public string get()
+        {
+            return "yeah";
+        }
+    }
+}
---
+++ publisherproject/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace publisherproject.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = null!;
+        public DateOnly PublishDate { get; set; }
+
+        //[Precision(7,2)]
+        public decimal BasePrice { get; set; }
+
+        //[Range(1,13,ErrorMessage ="mellem 1 til 13")]
+        //public int Rating { get; set; }
+        public Author? Author { get; set; }
+        public int AuthorId { get; set; }
+        //public Cover Cover { get; set; }
+    }
+}
